55c6e5bf31d2b5ac6610a3761a5b0678f4dcbee6
qvd808 <93557410+qvd808@users.noreply.github.com>
2022-02-19 12:38:23 -0800

    Lecture 2

--- Stormhacks/Form1.cs
+++ Stormhacks/Form1.cs
@@ -13,6 +13,7 @@ namespace Stormhacks
     public partial class Form1 : Form
     {
         Lecture1 lec1 = new Lecture1();
+        Lecture2 lec2 = new Lecture2();
         public Form1()
         {
             InitializeComponent();
@@ -27,6 +28,12 @@ namespace Stormhacks
         {
             this.Hide();
             lec1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            lec2.Show();
         }
     }
 }
