e4ef781040c9fc3a4d5e49409b8ae337fc65a841
knn3 <khoi.nguyen.8602@gmail.com>
2022-02-19 12:56:41 -0800

    Multiple choices

---
+++ Stormhacks/Practices1/MultipleChoice1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stormhacks.Practices1
+{
+    public partial class MultipleChoice1 : Form
+    {
+        public MultipleChoice1()
+        {
+            InitializeComponent();
+        }
+
+        private void A_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void D_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
