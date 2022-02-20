13f871a670bb3728078d8d1077d65ba353ee1f45
qvd808 <93557410+qvd808@users.noreply.github.com>
2022-02-19 18:04:06 -0800

    Practice 2

--- Stormhacks/Practices/Practice2.cs
+++ Stormhacks/Practices/Practice2.cs
@@ -16,5 +16,57 @@ namespace Stormhacks.Practices
         {
             InitializeComponent();
         }
+
+        private void submit1_Click(object sender, EventArgs e)
+        {
+            if(CorrectAns1.Checked == true)
+            {
+                Result1.Text = "Correct!!!";
+                submit1.Visible = false;
+            } else
+            {
+                Result1.Text = "Wrong! Try again";
+            }
+        }
+
+        private void submit2_Click(object sender, EventArgs e)
+        {
+            if (CorrectAns2.Checked == true)
+            {
+                Result2.Text = "Correct!!!";
+                submit2.Visible = false;
+            }
+            else
+            {
+                Result2.Text = "Wrong! Try again";
+            }
+        }
+
+        private void submit3_Click(object sender, EventArgs e)
+        {
+            if (CorrectAns3.Checked == true)
+            {
+                Result3.Text = "Correct!!!";
+                submit3.Visible = false;
+            }
+            else
+            {
+                Result3.Text = "Wrong! Try again";
+            }
+        }
+
+
+        private void submit4_Click(object sender, EventArgs e)
+        {
+            if (CorrectAns4.Checked == true)
+            {
+                Result4.Text = "Correct!!!";
+                submit4.Visible = false;
+            }
+            else
+            {
+                Result4.Text = "Wrong! Try again";
+            }
+        }
     }
 }
