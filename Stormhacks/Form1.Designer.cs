6b20f29e4df060583bc8eec02c257a96288d339b
deedeeNg <comga.nuocutuong@gmail.com>
2022-02-19 21:31:57 -0800

    Test

--- Stormhacks/Form1.Designer.cs
+++ Stormhacks/Form1.Designer.cs
@@ -39,10 +39,10 @@ namespace Stormhacks
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(129, 307);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(113, 230);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Lecture 1";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,10 +50,10 @@ namespace Stormhacks
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(129, 376);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(113, 282);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 31);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Lecture 2";
             this.button2.UseVisualStyleBackColor = true;
@@ -61,10 +61,10 @@ namespace Stormhacks
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(129, 447);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Location = new System.Drawing.Point(113, 335);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(86, 31);
+            this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Lecture 3";
             this.button3.UseVisualStyleBackColor = true;
@@ -72,10 +72,10 @@ namespace Stormhacks
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(129, 521);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Location = new System.Drawing.Point(113, 391);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(86, 31);
+            this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
             this.button4.Text = "Lecture 4";
             this.button4.UseVisualStyleBackColor = true;
@@ -83,10 +83,10 @@ namespace Stormhacks
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(129, 591);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.Location = new System.Drawing.Point(113, 443);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(86, 31);
+            this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
             this.button5.Text = "Lecture 5";
             this.button5.UseVisualStyleBackColor = true;
@@ -94,25 +94,26 @@ namespace Stormhacks
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(152, 112);
+            this.label1.Location = new System.Drawing.Point(133, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.Size = new System.Drawing.Size(32, 15);
             this.label1.TabIndex = 5;
             this.label1.Text = "UwU";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 857);
+            this.ClientSize = new System.Drawing.Size(324, 643);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Stormhacks/Homepage.Designer.cs
+++ Stormhacks/Homepage.Designer.cs
@@ -36,8 +36,9 @@ namespace Stormhacks
             // 
             // Start_button
             // 
+            this.Start_button.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Start_button.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Start_button.Location = new System.Drawing.Point(304, 146);
+            this.Start_button.Location = new System.Drawing.Point(321, 198);
             this.Start_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Start_button.Name = "Start_button";
             this.Start_button.Size = new System.Drawing.Size(82, 36);
@@ -48,8 +49,9 @@ namespace Stormhacks
             // 
             // Exit_button
             // 
+            this.Exit_button.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Exit_button.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Exit_button.Location = new System.Drawing.Point(304, 210);
+            this.Exit_button.Location = new System.Drawing.Point(321, 294);
             this.Exit_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Exit_button.Name = "Exit_button";
             this.Exit_button.Size = new System.Drawing.Size(82, 36);
@@ -60,9 +62,10 @@ namespace Stormhacks
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(247, 80);
+            this.label1.Location = new System.Drawing.Point(276, 95);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(182, 37);
             this.label1.TabIndex = 2;
@@ -72,12 +75,13 @@ namespace Stormhacks
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.ClientSize = new System.Drawing.Size(817, 531);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Exit_button);
             this.Controls.Add(this.Start_button);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Homepage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Homepage";
             this.Load += new System.EventHandler(this.Homepage_Load);
             this.ResumeLayout(false);
